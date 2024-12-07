877e4ad63cbcd5893e16263abb8fd1112fe77364
mikedistras <mike@distrasdesigns.com>
2024-12-07 16:32:55 +0000

    Update PUC to github

--- functions/PUC.php
+++ functions/PUC.php
@@ -4,15 +4,29 @@ use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 // Initialize the update checker without explicitly specifying context
 $myUpdateChecker = PucFactory::buildUpdateChecker(
-    'https://bitbucket.org/mikedistras/carerscount',
+    'https://github.com/Yalright/CarersCount',
     get_template_directory() . '/functions.php', // Path to the main theme file
-    'carerscount' // Theme slug (same as your theme folder name)
+    'carers-count' // Theme slug (same as your theme folder name)
 );
 
 // Optional: Set up authentication for private repositories
-if (defined('BITBUCKET_ACCESS_TOKEN')) {
-    $myUpdateChecker->setAuthentication(BITBUCKET_ACCESS_TOKEN);
-}
+
 
 // Optional: Specify the branch for stable releases
 $myUpdateChecker->setBranch('master');
+
+
+
+
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/Yalright/CarersCount',
+	get_template_directory() . '/functions.php',
+	'carers-count'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+// $myUpdateChecker->setAuthentication('your-token-here');
